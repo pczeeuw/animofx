8137292d8233d8929e6dca2afb1e0d9bab519d6f
Pieter <p.c.zeeuw@gmail.com>
2019-11-09 12:45:59 +0100

    initial commit

---
+++ src/main/java/nl/pczeeuw/animofx8/Animofx8Application.java
@@ -0,0 +1,13 @@
+package nl.pczeeuw.animofx8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Animofx8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Animofx8Application.class, args);
+    }
+
+}
